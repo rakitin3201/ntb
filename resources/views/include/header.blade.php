d3ba4cc3ef88d72d7687fbe9864df5606d9f21d5
Adama NIADA <a77272199@gmail.com>
2023-03-26 07:51:14 +0100

    Update navbar

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -11,27 +11,46 @@
 
             <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3 d-flex" role="search" method="POST" action="{{ route('search') }}">
                 {{ csrf_field() }}
-                <input type="search" class="form-control form-control-sm  mx-1" placeholder="Rechercher" aria-label="Search">
-                <button type="submit" class="btn btn-sm btn-light">Search</button>
+                <input class="form-control form-control-sm me-2" type="search" placeholder="Recherche" aria-label="Search">
+                <button class="btn btn-sm btn-outline-light" type="submit">Search</button>
             </form>
         </div>
     </div>
 </header>
 
-
-<div class="shadow-sm">
+<nav class="navbar navbar-expand-lg bg-light shadow-sm" aria-label="Offcanvas navbar large">
+{{-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark" aria-label="Offcanvas navbar large"> --}}
     <div class="container">
-        <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4">
-            <a href="{{ url('/') }}" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-                <img class="rounded-circle" width="50px" height="50px" src="{{ asset('images/img1.jpg') }}" alt="logo">
-            </a>
-
-            <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="{{ route('acceuil') }}" class="nav-link px-2 link-secondary">ACCEUIL</a></li>
-                <li><a href="{{ route('about') }}" class="nav-link px-2 link-dark">A PROPOS</a></li>
-                <li><a href="{{ route('service') }}" class="nav-link px-2 link-dark">SERVICES</a></li>
-                <li><a href="{{ route('contact') }}" class="nav-link px-2 link-dark">CONTACTS</a></li>
-            </ul>
-        </header>
+        <a class="navbar-brand" href="{{ url('/') }}">
+            <img class="rounded-circle" width="50px" height="50px" src="{{ asset('images/img1.jpg') }}" alt="logo">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar2" aria-controls="offcanvasNavbar2">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar2" aria-labelledby="offcanvasNavbar2Label">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasNavbar2Label">Offcanvas</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                    <li class="nav-item"><a class="nav-link active" aria-current="page" href="{{ route('acceuil') }}">ACCEUIL</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('about') }}">A PROPOS</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('service') }}">SERVICES</a></li>
+                    <li class="nav-item"><a class="nav-link" href="{{ route('contact') }}">CONTACTS</a></li>
+                    {{-- <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><hr class="dropdown-divider"></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li> --}}
+                </ul>
+            </div>
+        </div>
     </div>
-</div>
+</nav>
